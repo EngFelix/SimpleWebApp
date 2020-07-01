95930eb89d5342531207ee1dda977534b41b0229
EngFelix <felix.enghofer@tum.de>
2020-06-30 21:08:01 -0700

    refactoring of UserService

--- demo-ui/src/app/service/user.service.ts
+++ demo-ui/src/app/service/user.service.ts
@@ -14,8 +14,8 @@ export interface IUser {
 })
 export abstract class UserService {
   public abstract readonly users$: Observable<Array<IUser>>;
-  abstract updateUser(userId: number, userChanges: Partial<IUser>);
-  abstract createUser(user: IUser);
+  abstract updateUser(userId: number, userChanges: Partial<IUser>): void;
+  abstract createUser(user: IUser): void;
 }
 
 
@@ -24,8 +24,7 @@ export abstract class UserService {
 })
 export class UserServiceImpl implements UserService{
   private _users = new BehaviorSubject<Array<IUser>>([]);
-  private baseUrl = 'http://localhost:8080/api';
-//  private dataStore: { users: Array<IUser> } = {users: []};
+  private apiUrl = 'http://localhost:8080/api';
   readonly users$ = this._users.asObservable();
 
   constructor(private httpClient: HttpClient) {
@@ -33,7 +32,7 @@ export class UserServiceImpl implements UserService{
   }
 
   private loadAll(): void {
-    const getUsers$ = this.httpClient.get<Array<IUser>>(this.baseUrl + '/users')
+    const getUsers$ = this.httpClient.get<Array<IUser>>(this.apiUrl + '/users')
       .subscribe(
         data => this._users.next(data),
         error => console.log('UserService: error loading users', error)
@@ -43,7 +42,7 @@ export class UserServiceImpl implements UserService{
   }
 
   createUser(user: IUser): void {
-    this.httpClient.post<IUser>(this.baseUrl + '/users', user)
+    this.httpClient.post<IUser>(this.apiUrl + '/users', user)
       .subscribe(
         data => this._users.next([...this._users.getValue(), data]),
         error => console.log('UserService: error creating user', error)
@@ -54,7 +53,7 @@ export class UserServiceImpl implements UserService{
     const updatedUsers  = [...this._users.getValue()];
     const index = this._users.getValue().findIndex(user => user.id == userChanges.id);
 
-    this.httpClient.put<IUser>(this.baseUrl + '/users', userChanges)
+    this.httpClient.put<IUser>(this.apiUrl + '/users', userChanges)
       .subscribe(
         data => {
           updatedUsers[index] = data;
@@ -63,70 +62,4 @@ export class UserServiceImpl implements UserService{
         error => console.log('UserService: error creating user', error)
       );
   }
-
-
-
-  //
-  // private baseUrl = 'http://localhost:8080/api';
-  // private _users = new BehaviorSubject<Array<IUser>>([]);
-  // public readonly users$ : Observable<Array<IUser>> = this._users.asObservable();
-  //
-  // constructor(private httpClient:HttpClient) {
-  //   console.log('UserServiceImpl created! Loading all users from spring backend...')
-  //   this.loadAllUsers();
-  // }
-  //
-  // private loadAllUsers() {
-  //   const getUsers$ = this.httpClient.get<Array<IUser>>(this.baseUrl + '/users/')
-  //     // Observable
-  //     .pipe(
-  //       map(response => response['payload']),
-  //       catchError(err => {
-  //         console.log('Could not load users', err);
-  //         return throwError(err);
-  //       }),
-  //       tap(users => {
-  //         console.log(users);
-  //         this._users.next(users);
-  //       })
-  //     )
-  //
-  //
-  //
-  // }
-  //
-  // updateUser(userId: number, userChanges: Partial<IUser>): Observable<Array<IUser>> {
-  //   const users: Array<IUser> = this._users.getValue();
-  //   const index = users.findIndex(user => user.id == userId);
-  //   const newUser : IUser = {
-  //     ...users[index],
-  //     ...userChanges
-  //   }
-  //   const updatedUsers: Array<IUser> = users.slice(0);
-  //   updatedUsers[index] = newUser;
-  //   this._users.next(updatedUsers);
-  //   return this.httpClient.put<Array<IUser>>(this.baseUrl + '/users/' + userId, userChanges)
-  //     .pipe(
-  //       catchError(err => {
-  //         console.log('Could not update user', err);
-  //         return throwError(err);
-  //       }),
-  //       shareReplay()
-  //     )
-  // }
-  //
-  // createUser(user: IUser): Observable<Array<IUser>> {
-  //   return this.httpClient.post<Array<IUser>>(this.baseUrl + '/users', user)
-  //     .pipe(
-  //       map(response => response['payload']),
-  //       catchError(err => {
-  //         console.log('Could not create user', err);
-  //         return throwError(err);
-  //       }),
-  //       tap(users => this._users.next(users)),
-  //       shareReplay()
-  //     )
-  // }
-
-  // TODO: implement filter functions here?
 }
